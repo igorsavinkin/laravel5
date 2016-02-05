bbbcb671edf27fe0873acbf80536b3f4d3a8467e
igorsavinkin <igor.savinkin@gmail.com>
2016-02-05 15:07:07 +0200

    new commimits of laravelapp

--- testmysql.php
+++ testmysql.php
@@ -1,5 +1,5 @@
 <?php 
-$link = mysqli_connect('localhost','dbuser','dbpassword' , 'mysql'); 
+$link = mysqli_connect('localhost:3306', 'root', '' ); 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
